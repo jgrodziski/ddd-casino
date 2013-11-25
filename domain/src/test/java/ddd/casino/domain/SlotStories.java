075564f1fb75efff8f7046ddefd1bda81b55814b
Jérémie Grodziski <jeremie.github@grodziski.com>
2013-11-25 15:02:53 +0100

    initial commit

---
+++ domain/src/test/java/ddd/casino/domain/SlotStories.java
@@ -0,0 +1,20 @@
+package ddd.casino.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ */
+public class SlotStories extends Stories {
+
+    public SlotStories() {
+        super();
+    }
+
+    protected List<Object> mySteps() {
+        List<Object> steps = new ArrayList(1);
+        steps.add(new SlotSteps());
+        return steps;
+    }
+}
---
+++ domain/src/test/resources/play_with_slot.story
@@ -0,0 +1,2 @@
+Scenario: Play with Slot
+Given
